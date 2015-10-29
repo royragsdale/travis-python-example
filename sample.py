57be591dfe98823d6adedf5ae3e94546b278832f
Roy Ragsdale <roy@apexcno.com>
2015-10-29 15:34:41 -0400

    Initial setup - failing
    Added a simple program and some tests (including two that intentionally
    fail)
    
    Added travis file to kickoff continuous integration testing

---
+++ sample.py
@@ -0,0 +1,10 @@
+
+
+def add_and_one(a,b):
+    return a+b+1
+
+def double(a):
+    return 2*a
+
+def say_hello(a):
+    return "hello {}".format(a)
---
+++ test_sample.py
@@ -0,0 +1,29 @@
+from sample import add_and_one,double,say_hello
+
+# tests for add_and_one
+def test_add_and_one_2_3():
+    assert add_and_one(2,3) == 6
+
+def test_add_and_one_0_2():
+    assert add_and_one(0,2) == 2
+
+def test_add_negatives():
+    assert add_and_one(-1,-2) == -2
+
+# tests for doubler
+def test_double_0():
+    assert double(0) == 0
+
+def test_double_2():
+    assert double(2) == 4
+
+def test_double_negatives():
+    assert double(-2) == 4
+
+# tests for hello
+def test_say_hello_name():
+    assert say_hello("roy") == "hello roy"
+ 
+def test_say_hello_number():
+    assert say_hello(3) == "hello 3"
+ 
