17c6720e811ea1c8ae120558f7c91c50236a3f7d
Roy Ragsdale <roy@apexcno.com>
2016-09-20 15:01:50 -0400

    Add another function

--- sample.py
+++ sample.py
@@ -1,13 +1,21 @@
 from num2words import num2words
 
-def add_and_one(a,b):
+
+def add_and_one(a, b):
     return a+b+1
+
 
 def double(a):
     return 2*a
 
+
 def say_hello(a):
     return "hello {}".format(a)
 
+
 def num_speak(a):
     return num2words(a)
+
+
+def alwaysFalse(a):
+    return False
